346ba58c7659ed53c02456fe4053a30d37f1e9dc
cyan0714 <917244295@qq.com>
2024-07-21 16:06:30 +0800

    feat: user-module backend

---
+++ src/user/dto/register-user.dto.ts
@@ -0,0 +1,10 @@
+import { IsNotEmpty, MinLength } from "class-validator";
+
+export class RegisterUserDto {
+  @IsNotEmpty({ message: "username is required" })
+  username: string;
+
+  @IsNotEmpty({ message: "password is required" })
+  @MinLength(6, { message: "password must be at least 6 characters long" })
+  password: string;
+}
